6256c29204e7889d1fccbf8678af24755cdce9c3
Eero Helenius <eero.helenius@citec.com>
2016-06-18 00:37:55 +0300

    Replace "in" with ">>" in example spec

--- examples/src/test/scala/ExampleSpecification.scala
+++ examples/src/test/scala/ExampleSpecification.scala
@@ -31,7 +31,7 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
       * This is equivalent to calling <xsl:apply-templates/> on the input <paragraph> element and checking that the
       * output is a <p> element with the same content.
       */
-    "Simple transformation" in {
+    "Simple transformation" >> {
         applying(<paragraph>foo</paragraph>) must produce(<p>foo</p>)
     }
 
@@ -46,7 +46,7 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
      * </xsl:apply-templates>
      * }}}
      */
-    "Set mode and parameters" in {
+    "Set mode and parameters" >> {
         applying(<paragraph>foo</paragraph>)
             .withMode("main")
             .withParameters(tunnel = false, "foo" -> "bar", "baz" -> "quux") must produce(<p>bar, quux</p>)
@@ -56,7 +56,7 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
       *
       * You must define the `@as` attribute on your `<xsl:template>` element for this to work.
       */
-    "Apply a template that returns an atomic value" in {
+    "Apply a template that returns an atomic value" >> {
         // <-> is an alias for "produce".
         applying(<anyElement/>).withMode("returns-atomic-value") must <->(6)
     }
@@ -65,7 +65,7 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
       * of your target node. It will be set as the context item for the transformation. You can then supply an XPath
       * query that selects the node you want to apply the templates for.
       */
-    "Apply a template that accesses an ancestor node" in {
+    "Apply a template that accesses an ancestor node" >> {
         applying(
             <ancestor copied="value"><descendant/></ancestor>,
             XPath.select("ancestor/descendant")
@@ -75,12 +75,12 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
     }
 
     /** Call a function and check the result. */
-    "Call a function that returns an atomic value" in {
+    "Call a function that returns an atomic value" >> {
         // You can either pass the namespace URI of the function explicitly...
         callingFunction("local", "increment")(1) must produce(2)
     }
 
-    "Call a function that takes an element and returns a document node" in {
+    "Call a function that takes an element and returns a document node" >> {
         // ...or you can define an implicit namespace in your specification so that you don't have to.
         callingFunction("wrap-into-foo")(element(<bar/>)) must produce(<foo><bar/></foo>)
     }
@@ -88,42 +88,42 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
     /** If your function takes or returns a node type that cannot be represented by Scala's XML literals, you can use
       * the supplied constructors to give and match those node types.
       */
-    "Call a function that takes and returns an attribute" in {
+    "Call a function that takes and returns an attribute" >> {
         callingFunction("rename-to-baz")(attribute("foo" -> "bar")) must produce(attribute("baz" -> "quux"))
     }
 
-    "Call a function that takes and returns a text node" in {
+    "Call a function that takes and returns a text node" >> {
         callingFunction("take-and-return-text")(text("foo")) must produce(text("bar"))
     }
 
     /** Check templates and functions that take or return a sequence with mixed types. */
-    "Call a function that takes mixed parameters" in {
+    "Call a function that takes mixed parameters" >> {
         callingFunction("mix")(1, element(<foo/>), "bar") must produce("bar", element(<foo/>), 1)
     }
 
     /** Any parametes you set by overriding the "parameters" val are available for all tests in this specification. */
-    "Call a named template that accesses global parameters" in {
+    "Call a named template that accesses global parameters" >> {
         callingTemplate("return-global-params") must produce(1, "parameter", new URI("http://www.dita-ot.org").toString)
     }
 
-    "Call a named template that returns an atomic value" in {
+    "Call a named template that returns an atomic value" >> {
         callingTemplate("sum").withParameters(tunnel = false, "a" -> 1, "b" -> 2) must produce(3)
     }
 
-    "Call a named template and with a context node" in {
+    "Call a named template and with a context node" >> {
         callingTemplate("a-to-b", contextNode = <a x="y"/>) must produce(<b x="y"/>)
     }
 
     /** If your XSLT code calls the `doc()` or `document()` XPath functions, you can create temporary mock XML files
       * that only exist for the duration of the test. They are stored in memory, not on your actual file system.
       */
-    "Apply a template that calls doc()" in withFiles(MockFile("b.xml", <foo/>)) {
+    "Apply a template that calls doc()" >> withFiles(MockFile("b.xml", <foo/>)) {
         applying(<include href="b.xml"/>) must produce(<foo/>)
     }
 
     /** If your XML content has randomly generated content or bits you don't care about, you can configure the test to
       * ignore certain attributes or elements. */
-    "Ignore an attribute" in {
+    "Ignore an attribute" >> {
         // Define a filter that ignores the `@id` attribute.
         val af = filter[Attr](a => a.getName != "id")
         val m = (s: Source) => defaultMatcher(s).withAttributeFilter(af)
@@ -132,7 +132,7 @@ class ExampleSpecification extends mutable.Specification with XsltSpecification 
     }
 
     /** Documents created with `<xsl:result-document>` are also stored in the in-memory file system. */
-    "<xsl:result-document> creates a file in the transient file system" in {
+    "<xsl:result-document> creates a file in the transient file system" >> {
         applying(<paragraph>foo</paragraph>).withMode("result-document")
             .result must beEmpty and (TransientFileSystem.hasFile(fileSystem, "foo.xml") must beTrue)
     }.pendingUntilFixed("<https://saxonica.plan.io/issues/2771>")
