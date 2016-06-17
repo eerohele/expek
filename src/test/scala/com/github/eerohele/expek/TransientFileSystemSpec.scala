7af476a0992fe89a7385614dca1866c1b8a0ab9d
Eero Helenius <eero.helenius@citec.com>
2016-06-18 00:37:38 +0300

    Add TFS specs

--- src/test/scala/com/github/eerohele/expek/TransientFileSystemSpec.scala
+++ src/test/scala/com/github/eerohele/expek/TransientFileSystemSpec.scala
@@ -1,24 +1,57 @@
 package com.github.eerohele.expek
 
 import java.net.URI
+import java.io.File
 
 import org.specs2.mutable.Specification
 import com.github.eerohele.expek.{TransientFileSystem => TFS}
 import com.google.common.jimfs.Jimfs
 
+// scalastyle:off multiple.string.literals
+
 class TransientFileSystemSpec extends Specification {
     lazy val fs = Jimfs.newFileSystem
 
-    "Getting the root path of the file system" should {
-        "return the root Path of the given file system" in {
-            val uri = TFS.getRoot(fs).toUri
-            uri.getPath must be_==("/") and(uri.getScheme must be_==(Jimfs.URI_SCHEME))
+    "Getting the root path of the file system" >> {
+        val uri = TFS.getRoot(fs).toUri
+        uri.getPath must be_==("/") and(uri.getScheme must be_==(Jimfs.URI_SCHEME))
+    }
+
+    "Converting URI into a Path on the file system" >> {
+        TFS.pathFromURI(fs, new URI("foo")).toUri.getPath must be_==("/work/foo")
+    }
+
+    "Converting an Elem into a Source" should {
+        "set the base URI of the source to point to the file system" in {
+            new URI(TFS.source(fs, <foo/>).getSystemId).getScheme must be_==(Jimfs.URI_SCHEME)
         }
     }
 
-    "Turning URI into a Path" should {
-        "work" in {
-            TFS.pathFromURI(fs, new URI("foo")).toUri.getPath must be_==("/work/foo")
+    "Creating a path for an Elem on the file system" should {
+        val uri = new URI(TFS.makeTransientPath(fs, <foo/>))
+
+        "Return a path in the root of the file system" in {
+            new File(uri.getPath).getParent must be_==("/")
+        }
+
+        "Have the Jimfs URI scheme" in {
+            uri.getScheme must be_==(Jimfs.URI_SCHEME)
+        }
+
+        "Have the .xml extension" in {
+            uri.toString must endWith(".xml")
+        }
+    }
+
+    "Checking whether a file exists" should {
+        "return false if the file doesn't exist" in {
+            TFS.hasFile(fs, "/foo.xml") must beFalse
+        }
+
+        "return true if the file doesn't exist" in {
+            val m = MockFile("foo.xml", <bar/>)(fs)
+            m.create
+            TFS.hasFile(fs, "/foo.xml") must beTrue
         }
     }
 }
