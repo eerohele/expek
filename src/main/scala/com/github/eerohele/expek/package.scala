d59898bca771429221b1d2f30bca49566db5d92f
Eero Helenius <eero.helenius@citec.com>
2016-06-19 21:08:08 +0300

    Fix type signature of tap() argument

--- src/main/scala/com/github/eerohele/expek/package.scala
+++ src/main/scala/com/github/eerohele/expek/package.scala
@@ -2,6 +2,6 @@ package com.github.eerohele.expek
 
 package object utils {
     private[expek] implicit class Tap[A](private val a: A) extends AnyVal {
-        def tap[B](f: A => B): A = { f(a); a }
+        def tap[B](f: A => Unit): A = { f(a); a }
     }
 }
