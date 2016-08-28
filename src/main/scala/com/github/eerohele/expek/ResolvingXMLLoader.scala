790874204cf147375df7593ac6da794d1ce3ff61
Eero Helenius <eero.helenius@kapsi.fi>
2016-08-28 08:32:57 +0300

    Add ResolvingXMLLoader class
    Convert an XML file into a scala.xml.Node with XML catalog resolving.

---
+++ src/main/scala/com/github/eerohele/expek/ResolvingXMLLoader.scala
@@ -0,0 +1,41 @@
+package com.github.eerohele.expek
+
+import java.io.File
+import java.net.URL
+
+import com.sun.org.apache.xml.internal.resolver.tools.CatalogResolver
+
+import scala.xml.factory.XMLLoader
+import scala.xml.parsing.NoBindingFactoryAdapter
+import scala.xml.{Elem, Node}
+
+/* Functions for converting an XML file into a [[Node]] with catalog resolving enabled.
+ *
+ * You must set the `xml.catalog.files` system property for this to work at all.
+ *
+ * Code 100% stolen from http://horstmann.com/unblog/2011-12-12/catalog.html.
+ */
+object ResolvingXMLLoader {
+    private lazy val resolver = new CatalogResolver
+
+    private lazy val loader = new XMLLoader[Elem] {
+        override def adapter = new NoBindingFactoryAdapter {
+            override def resolveEntity(publicId: String, systemId: String) = {
+                resolver.resolveEntity(publicId, systemId)
+            }
+        }
+    }
+
+    def load(url: URL): Node = loader.load(url)
+
+    /** Load the given [[File]] with catalog resolving enabled.
+      *
+      * Example:
+      *
+      * {{{
+      * System.setProperty("xml.catalog.files", "/path/to/catalog.xml")
+      * ResolvingXMLLoader.load(new File("foo.xml"))
+      * }}}
+      */
+    def load(file: File): Node = load(file.toURI.toURL)
+}
